b5fd91bacc9131e525a3a441103208d804c657ed
Martin Schneider <martin.schneider2001@gmail.com>
2021-12-22 09:38:32 +0100

    1

--- LifeGame.py
+++ LifeGame.py
@@ -66,85 +66,81 @@ def newGeneration(matrice, rows):
                 else:
                     newMatrice[i][j] = 0
     return newMatrice
-    
-def initialise(width, rows, surface, pcVie):
-    matrice = initMatrice(rows, pcVie)
-    myGrid = drawGrid(width, rows, surface)
+
+
+def initialise():
+    matrice = initMatrice(scale1.get(), scale2.get())
+    myGrid = drawGrid(600, rows, surface)
     fillGrid(matrice, myGrid)
 
 
-def main():
-    windowWidth = 800
-    windowHeight = 600
-    rows = 20
-    window = Tk()
-    window.title('SR01 Jeu de la vie')
-    window.geometry(f"{windowWidth}x{windowHeight}")
+windowWidth = 800
+windowHeight = 600
+rows = 20
+window = Tk()
+window.title('SR01 Jeu de la vie')
+window.geometry(f"{windowWidth}x{windowHeight}")
 
-    gridFrame = Frame(window)
-    gridFrame.pack(side=LEFT)
+gridFrame = Frame(window)
+gridFrame.pack(side=LEFT)
 
-    rightFrame = Frame(window, width=200, height=600, bg='#DCDCDC')
-    rightFrame.pack_propagate(False)
-    rightFrame.pack(side=RIGHT)
+rightFrame = Frame(window, width=200, height=600, bg='#DCDCDC')
+rightFrame.pack_propagate(False)
+rightFrame.pack(side=RIGHT)
 
-    buttonFrame = Frame(rightFrame, width=200, bg='#DCDCDC')
-    buttonFrame.pack_propagate(False)
-    buttonFrame.pack(side=TOP)
+buttonFrame = Frame(rightFrame, width=200, bg='#DCDCDC')
+buttonFrame.pack_propagate(False)
+buttonFrame.pack(side=TOP)
 
-    textFrame = Frame(rightFrame, width=200, bg='#DCDCDC')
-    textFrame.pack_propagate(False)
-    textFrame.pack(side=BOTTOM)
+textFrame = Frame(rightFrame, width=200, bg='#DCDCDC')
+textFrame.pack_propagate(False)
+textFrame.pack(side=BOTTOM)
 
-    text1 = Label(textFrame, text='Taille de la grille', fg='#22427C')
-    text1.pack()
-    text1.grid(row=0)
+text1 = Label(textFrame, text='Taille de la grille', fg='#22427C')
+text1.pack()
+text1.grid(row=0)
 
-    scale1 = Scale(textFrame, orient=HORIZONTAL,
-                   fg='#22427C', from_=10, to=100)
-    scale1.pack()
-    scale1.grid(row=1)
+scale1 = Scale(textFrame, orient=HORIZONTAL,
+                fg='#22427C', from_=10, to=100)
+scale1.pack()
+scale1.grid(row=1)
 
-    text2 = Label(textFrame, text='% de Vie', fg='#22427C')
-    text2.pack()
-    text2.grid(row=2)
+text2 = Label(textFrame, text='% de Vie', fg='#22427C')
+text2.pack()
+text2.grid(row=2)
 
-    scale2 = Scale(textFrame, orient=HORIZONTAL,
-                   fg='#22427C', from_=0, to=100)
-    scale2.pack()
-    scale2.grid(row=3)
+scale2 = Scale(textFrame, orient=HORIZONTAL,
+                fg='#22427C', from_=0, to=100)
+scale2.pack()
+scale2.grid(row=3)
 
-    text3 = Label(textFrame, text='Vitesse', fg='#22427C')
-    text3.pack()
-    text3.grid(row=4)
+text3 = Label(textFrame, text='Vitesse', fg='#22427C')
+text3.pack()
+text3.grid(row=4)
 
-    vitesse = IntVar()
+vitesse = IntVar()
 
-    scale3 = Scale(textFrame, orient=HORIZONTAL,
-                   fg='#22427C', from_=2, to=20, variable=vitesse)
-    scale3.pack()
-    scale3.grid(row=5)
+scale3 = Scale(textFrame, orient=HORIZONTAL,
+                fg='#22427C', from_=2, to=20, variable=vitesse)
+scale3.pack()
+scale3.grid(row=5)
 
-    button4 = Button(textFrame, text='Quitter', bg='#C0C0C0',
-                     fg='#22427C', command=window.quit)
-    button4.pack(fill=X)
-    button4.grid(row=6, sticky='nesw')
+button4 = Button(textFrame, text='Quitter', bg='#C0C0C0',
+                    fg='#22427C', command=window.quit)
+button4.pack(fill=X)
+button4.grid(row=6, sticky='nesw')
 
-    button1 = Button(buttonFrame, text='Lancer', bg='#C0C0C0', fg='#22427C')
-    button1.pack(fill=X)
-    button1.grid(row=0)
+button1 = Button(buttonFrame, text='Lancer', bg='#C0C0C0', fg='#22427C')
+button1.pack(fill=X)
+button1.grid(row=0)
 
-    button2 = Button(buttonFrame, text='Arreter', bg='#C0C0C0', fg='#22427C')
-    button2.pack(fill=X)
-    button2.grid(row=1)
+button2 = Button(buttonFrame, text='Arreter', bg='#C0C0C0', fg='#22427C')
+button2.pack(fill=X)
+button2.grid(row=1)
 
-    button3 = Button(buttonFrame, text='Initialiser',
-                     bg='#C0C0C0', fg='#22427C', command=initialise(600, 20, gridFrame, 0.2))
-    button3.pack(fill=X)
-    button3.grid(row=2)
+button3 = Button(buttonFrame, text='Initialiser',
+                    bg='#C0C0C0', fg='#22427C', command=initialise)
+button3.pack(fill=X)
+button3.grid(row=2)
 
-    window.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+window.mainloop()
